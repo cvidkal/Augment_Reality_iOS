19eabcecd34b05634e8c4190db3835a82c44a07a
Shangjin Zhai <hotwolf@ShangjindeMac-mini.local>
2014-11-17 16:14:21 +0800

    Initial Commit

---
+++ newAR/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  newAR
+//
+//  Created by Shangjin Zhai on 14-11-17.
+//  Copyright (c) 2014年 Shangjin Zhai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ newAR/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  newAR
+//
+//  Created by Shangjin Zhai on 14-11-17.
+//  Copyright (c) 2014年 Shangjin Zhai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
